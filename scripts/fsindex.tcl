6251727fc05620e0ed0af8c9d4de4a36dbc05a0d
Wojciech Kocjan <683972+wojciechka@users.noreply.github.com>
2011-11-22 11:54:52 +0000

    2011-11-22 Wojciech Kocjan <zoro2@users.sourceforge.net>         * scripts/fsindex.tcl: Fix for utime command handling in pure Tcl version         * tests/fsindex.test:         * scripts/vfs.tcl:

--- scripts/fsindex.tcl
+++ scripts/fsindex.tcl
@@ -15,9 +15,9 @@ proc cookfs::fsindex {{data ""}} {
     array set cm {}
 
     if {$data != ""} {
-	fsindex::import $name $data
+         fsindex::import $name $data
     }  else  {
-	array set c {"" {}}
+         array set c {"" {}}
     }
 
     interp alias {} $name {} ::cookfs::fsindex::handle $name
@@ -27,7 +27,7 @@ proc cookfs::fsindex {{data ""}} {
 proc cookfs::fsindex::_normalizePath {path} {
     set rc [join [file split $path] /]
     if {$rc == "."} {
-	set rc ""
+         set rc ""
     }
     return $rc
 }
@@ -36,7 +36,7 @@ proc cookfs::fsindex::upvarPath {name path varName} {
     upvar #0 $name c
     set path [_normalizePath $path]
     if {![info exists c($path)]} {
-	error "Not found"
+         error "Not found"
     }
     uplevel 1 [list upvar #0 ${name}($path) $varName]
 }
@@ -46,7 +46,7 @@ proc cookfs::fsindex::upvarPathDir {name path varName tailVarName} {
     uplevel 1 [list set $tailVarName [file tail $path]]
     set dpath [_normalizePath [file dirname $path]]
     if {![info exists c($dpath)]} {
-	error "Not found"
+         error "Not found"
     }
     uplevel 1 [list upvar #0 ${name}($dpath) $varName]
 }
@@ -55,7 +55,7 @@ proc cookfs::fsindex::entriesList {name path} {
     upvarPath $name $path d
     set rc {}
     foreach {n v} $d {
-	lappend rc $n
+         lappend rc $n
     }
     return [lsort $rc]
 }
@@ -66,7 +66,7 @@ proc cookfs::fsindex::entryGet {name path} {
     array set da $d
     #puts "GET $path ([info exists da($tail)])"
     if {![info exists da($tail)]} {
-	error "Entry not found"
+         error "Entry not found"
     }
     return $da($tail)
 }
@@ -80,62 +80,73 @@ proc cookfs::fsindex::entrySet {name path data} {
     # TODO: validate entry - parent has to be existing directory
     upvar #0 $name c
     if {[catch {
-	upvarPathDir $name $path d tail
+         upvarPathDir $name $path d tail
     }]} {
-	error "Unable to create entry"
+         error "Unable to create entry"
     }
     array set da $d
     if {[llength $data] == 2} {
-	set size 0
-	foreach {- - csize} [lindex $data 1] {
-	    incr size $csize
-	}
-	set data [list [lindex $data 0] $size [lindex $data 1]]
+         set size 0
+         foreach {- - csize} [lindex $data 1] {
+             incr size $csize
+         }
+         set data [list [lindex $data 0] $size [lindex $data 1]]
     }
 
     if {![info exists da($tail)]} {
-	lappend d $tail $data
-	if {[llength $data] == 1} {
-	    # directory
-	    set path [_normalizePath $path]
-	    set c($path) {}
-	}
+         lappend d $tail $data
+         if {[llength $data] == 1} {
+             # directory
+             set path [_normalizePath $path]
+             set c($path) {}
+         }
     }  else  {
-	if {[llength $da($tail)] != [llength $data]} {
-	    error "Type mismatch"
-	}
-	set da($tail) $data
-	set d [array get da]
+         if {[llength $da($tail)] != [llength $data]} {
+             error "Type mismatch"
+         }
+         set da($tail) $data
+         set d [array get da]
     }
 }
 
 proc cookfs::fsindex::entrySetmtime {name path mtime} {
     if {[catch {
-	upvarPathDir $name $path d tail
+         upvarPathDir $name $path d tail
     }]} {
-	error "Unable to create entry"
+         error "Unable to create entry"
     }
     array set da $d
     if {![info exists da($tail)]} {
-	error "Entry not found"
+         error "Entry not found"
     }  else  {
-	lset da($tail) 0 $mtime
+         lset da($tail) 0 $mtime
         set d [array get da]
     }
 }
 
 proc cookfs::fsindex::entryUnset {name path} {
     if {[catch {
-	upvarPathDir $name $path d tail
+         upvarPathDir $name $path d tail
     }]} {
-	error "Unable to unset item"
+         error "Unable to unset item"
     }
     array set da $d
     if {[info exists da($tail)]} {
+        # check if an entry is non-empty directory
+        if {[llength $da($tail)] == 1} {
+            if {[catch {
+                upvarPath $name $path dl
+            }]} {
+                error "Unable to unset item"
+            }
+            if {[llength $dl] > 0} {
+                error "Unable to unset item"
+            }
+        }
         unset da($tail)
         set d [array get da]
     }  else  {
-	error "Unable to unset item"
+         error "Unable to unset item"
     }
 }
 
@@ -145,7 +156,7 @@ proc cookfs::fsindex::cleanup {name} {
 proc cookfs::fsindex::import {name data} {
     upvar #0 $name c
     if {[string range $data 0 7] != "CFS2.200"} {
-	error "Unable to create index object"
+         error "Unable to create index object"
     }
     set data [string range $data 8 end]
     importPath $name {} data
@@ -156,34 +167,34 @@ proc cookfs::fsindex::importPath {name path varname} {
     upvar #0 $name c
     upvar 1 $varname data
     if {[binary scan $data Ia* numitems data] != 2} {
-	error "Unable to create index object"
+         error "Unable to create index object"
     }
     set c($path) {}
     for {set i 0} {$i < $numitems} {incr i} {
-	if {[binary scan $data ca* numchars data] != 2} {
-	    error "Unable to create index object"
-	}
-	set filename [string range $data 0 [expr {$numchars - 1}]]
-	set data [string range $data [expr {$numchars + 1}] end]
-	set filename [encoding convertfrom utf-8 $filename]
-	if {[binary scan $data WIa* time numblocks data] != 3} {
-	    error "Unable to create index object"
-	}
-	set pathname [_normalizePath [file join $path $filename]]
-	if {$numblocks == -1} {
-	    importPath $name $pathname data
-	    lappend c($path) $filename [list $time]
-	}  else  {
-	    set numdata [expr {$numblocks * 3}]
-	    if {[binary scan $data I${numdata}a* bosdata data] != 2} {
-		error "Unable to create index object"
-	    }
-	    set size 0
-	    foreach {- - cs} $bosdata {
-		incr size $cs
-	    }
-	    lappend c($path) $filename [list $time $size $bosdata]
-	}
+         if {[binary scan $data ca* numchars data] != 2} {
+             error "Unable to create index object"
+         }
+         set filename [string range $data 0 [expr {$numchars - 1}]]
+         set data [string range $data [expr {$numchars + 1}] end]
+         set filename [encoding convertfrom utf-8 $filename]
+         if {[binary scan $data WIa* time numblocks data] != 3} {
+             error "Unable to create index object"
+         }
+         set pathname [_normalizePath [file join $path $filename]]
+         if {$numblocks == -1} {
+             importPath $name $pathname data
+             lappend c($path) $filename [list $time]
+         }  else  {
+             set numdata [expr {$numblocks * 3}]
+             if {[binary scan $data I${numdata}a* bosdata data] != 2} {
+                  error "Unable to create index object"
+             }
+             set size 0
+             foreach {- - cs} $bosdata {
+                  incr size $cs
+             }
+             lappend c($path) $filename [list $time $size $bosdata]
+         }
     }
 }
 
@@ -200,16 +211,16 @@ proc cookfs::fsindex::exportPath {name path} {
     set rc ""
     append rc [binary format I [llength [array names da]]]
     foreach n [array names da] {
-	set un [encoding convertto utf-8 $n]
-	append rc [binary format c [string length $un] ] $un \u0000
-	append rc [binary format W [lindex $da($n) 0]]
-	if {[llength $da($n)] == 1} {
-	    append rc [binary format I -1]
-	    append rc [exportPath $name [file join $path $n]]
-	}  else  {
-	    set numblocks [expr {[llength [lindex $da($n) 2]] / 3}]
-	    append rc [binary format II* $numblocks [lindex $da($n) 2]]
-	}
+         set un [encoding convertto utf-8 $n]
+         append rc [binary format c [string length $un] ] $un \u0000
+         append rc [binary format W [lindex $da($n) 0]]
+         if {[llength $da($n)] == 1} {
+             append rc [binary format I -1]
+             append rc [exportPath $name [file join $path $n]]
+         }  else  {
+             set numblocks [expr {[llength [lindex $da($n) 2]] / 3}]
+             append rc [binary format II* $numblocks [lindex $da($n) 2]]
+         }
     }
     return $rc
 }
@@ -218,22 +229,22 @@ proc cookfs::fsindex::importMetadata {name varname} {
     upvar #0 $name.m cm
     upvar $varname data
     if {([string length $data] >= 4) && ([binary scan $data I count] > 0)} {
-	set data [string range $data 4 end]
-	for {set i 0} {$i < $count} {incr i} {
-	    if {![binary scan $data I size]} {
-		set data {}
-		return
-	    }
-	    set bindata [string range $data 4 [expr {$size + 3}]]
-	    set si [string first \0 $bindata]
+         set data [string range $data 4 end]
+         for {set i 0} {$i < $count} {incr i} {
+             if {![binary scan $data I size]} {
+                  set data {}
+                  return
+             }
+             set bindata [string range $data 4 [expr {$size + 3}]]
+             set si [string first \0 $bindata]
 
-	    if {$si >= 0} {
-		set key [string range $bindata 0 [expr {$si - 1}]]
-		set value [string range $bindata [expr {$si + 1}] end]
-		set cm($key) $value
-	    }
-	    set data [string range $data [expr {$size + 4}] end]
-	}
+             if {$si >= 0} {
+                  set key [string range $bindata 0 [expr {$si - 1}]]
+                  set value [string range $bindata [expr {$si + 1}] end]
+                  set cm($key) $value
+             }
+             set data [string range $data [expr {$size + 4}] end]
+         }
     }
 }
 
@@ -243,9 +254,9 @@ proc cookfs::fsindex::exportMetadata {name} {
     set nl [array names cm]
     set rc [binary format I [llength $nl]]
     foreach n $nl {
-	set v $cm($n)
-	set size [expr {[string length $n] + [string length $v] + 1}]
-	append rc [binary format I $size] $n \0 $v
+         set v $cm($n)
+         set size [expr {[string length $n] + [string length $v] + 1}]
+         append rc [binary format I $size] $n \0 $v
     }
     return $rc
 }
@@ -253,11 +264,11 @@ proc cookfs::fsindex::exportMetadata {name} {
 proc cookfs::fsindex::getMetadata {name paramname valuevariable} {
     upvar #0 $name.m cm
     if {[info exists cm($paramname)]} {
-	upvar 1 $valuevariable v
-	set v $cm($paramname)
-	return 1
+         upvar 1 $valuevariable v
+         set v $cm($paramname)
+         return 1
     }  else  {
-	return 0
+         return 0
     }
 }
 
@@ -274,74 +285,74 @@ proc cookfs::fsindex::unsetMetadata {name paramname} {
 proc cookfs::fsindex::handle {name cmd args} {
     #puts "handle $name $cmd $args"
     switch -- $cmd {
-	export {
-	    return [export $name]
-	}
-	list {
-	    if {([llength $args] == 1)} {
-		return [entriesList $name [lindex $args 0]]
-	    }
-	}
-	get {
-	    if {([llength $args] == 1)} {
-		return [entryGet $name [lindex $args 0]]
-	    }
-	}
-	getmtime {
-	    if {([llength $args] == 1)} {
-		return [entryGetmtime $name [lindex $args 0]]
-	    }
-	}
-	set {
-	    if {([llength $args] == 2) || ([llength $args] == 3)} {
-		# directory: path mtime
-		entrySet $name [lindex $args 0] [lrange $args 1 end]
-	    }
-	    return ""
-	}
-	setmtime {
-	    if {[llength $args] == 2} {
-		entrySetmtime $name [lindex $args 0] [lindex $args 1]
+         export {
+             return [export $name]
+         }
+         list {
+             if {([llength $args] == 1)} {
+                  return [entriesList $name [lindex $args 0]]
+             }
+         }
+         get {
+             if {([llength $args] == 1)} {
+                  return [entryGet $name [lindex $args 0]]
+             }
+         }
+         getmtime {
+             if {([llength $args] == 1)} {
+                  return [entryGetmtime $name [lindex $args 0]]
+             }
+         }
+         set {
+             if {([llength $args] == 2) || ([llength $args] == 3)} {
+                  # directory: path mtime
+                  entrySet $name [lindex $args 0] [lrange $args 1 end]
+             }
+             return ""
+         }
+         setmtime {
+             if {[llength $args] == 2} {
+                  entrySetmtime $name [lindex $args 0] [lindex $args 1]
                 return ""
-	    }
-	}
-	unset {
-	    if {[llength $args] == 1} {
-		entryUnset $name [lindex $args 0]
+             }
+         }
+         unset {
+             if {[llength $args] == 1} {
+                  entryUnset $name [lindex $args 0]
                 return ""
-	    }
-	}
-	delete {
-	    cleanup $name
-	    unset $name
-	    unset $name.m
-	    rename $name ""
-	    return ""
-	}
-	getmetadata {
-	    if {([llength $args] == 1) || ([llength $args] == 2)} {
-		# name ?defaultValue?
-		set value [lindex $args 1]
-		if {![getMetadata $name [lindex $args 0] value]} {
-		    if {[llength $args] == 1} {
-			error "Parameter not defined"
-		    }
-		}
-		return $value
-	    }
-	}
-	setmetadata {
-	    if {[llength $args] == 2} {
-		setMetadata $name [lindex $args 0] [lindex $args 1]
-		return ""
-	    }
-	}
-	unsetmetadata {
-	    if {[llength $args] == 1} {
-		unsetMetadata $name [lindex $args 0]
-		return ""
-	    }
-	}
+             }
+         }
+         delete {
+             cleanup $name
+             unset $name
+             unset $name.m
+             rename $name ""
+             return ""
+         }
+         getmetadata {
+             if {([llength $args] == 1) || ([llength $args] == 2)} {
+                  # name ?defaultValue?
+                  set value [lindex $args 1]
+                  if {![getMetadata $name [lindex $args 0] value]} {
+                      if {[llength $args] == 1} {
+                           error "Parameter not defined"
+                      }
+                  }
+                  return $value
+             }
+         }
+         setmetadata {
+             if {[llength $args] == 2} {
+                  setMetadata $name [lindex $args 0] [lindex $args 1]
+                  return ""
+             }
+         }
+         unsetmetadata {
+             if {[llength $args] == 1} {
+                  unsetMetadata $name [lindex $args 0]
+                  return ""
+             }
+         }
     }
     error "TODO: args"
 }
