d67e58043a6ceed4a9b043290502ce4ff9ff93c7
Konstantin Kushnir <chpock@gmail.com>
2024-05-26 00:50:41 +0000

    Fix build without c-pages, but with c-fsindex

--- generic/fsindexIO.h
+++ generic/fsindexIO.h
@@ -8,7 +8,9 @@
 
 Tcl_Obj *Cookfs_FsindexToObject(Cookfs_Fsindex *i);
 Cookfs_Fsindex *Cookfs_FsindexFromObject(Cookfs_Fsindex *i, Tcl_Obj *o);
+#ifdef COOKFS_USECPAGES
 Cookfs_Fsindex *Cookfs_FsindexFromPages(Cookfs_Fsindex *fsindex, Cookfs_Pages *pages);
+#endif /* COOKFS_USECPAGES */
 
 #endif /* COOKFS_USECFSINDEX */
 
